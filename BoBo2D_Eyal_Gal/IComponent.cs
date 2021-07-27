d291ea70a3404093c7a8ab899dc39ecf483bd56c
Daimnon <galdrorerez@gmail.com>
2021-07-27 04:51:12 +0300

    BoBo2D First Checkpoint

---
+++ BoBo2D_Eyal_Gal/IComponent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoBo2D_Eyal_Gal
+{
+    interface IComponent
+    {
+    }
+}
---
+++ BoBo2D_Eyal_Gal/SaveData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoBo2D_Eyal_Gal
+{
+    public class SaveData
+    {
+        public int id = 5;
+        //all data that needs to be saved should come from here
+    }
+}
