f250ae9f4d3fc12d3bbc0e45e84a0d2a3ebb2d90
Daimnon <galdrorerez@gmail.com>
2021-08-09 09:13:55 +0300

    Changes to Sprite files

--- BoBo2D_Eyal_Gal/Game1.cs
+++ BoBo2D_Eyal_Gal/Game1.cs
@@ -50,9 +50,9 @@ namespace BoBo2D_Eyal_Gal
                 Exit();
 
             // TODO: Add your update logic here
+            SubscriptionManager.ActivateAllSubscribersOfType<ICollidable>();
             Physics.SolveCollision();
             //Physics.SolveIntersection();
-            SubscriptionManager.ActivateAllSubscribersOfType<ICollidable>();
             _activeScene.Update();
             base.Update(gameTime);
         }
