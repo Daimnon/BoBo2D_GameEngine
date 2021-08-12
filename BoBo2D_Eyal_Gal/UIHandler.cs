ce76280e565c12feea1f576bc4f035dba85fa1c7
EDeyal <75429849+EDeyal@users.noreply.github.com>
2021-08-12 14:01:09 +0300

    UI handler

---
+++ BoBo2D_Eyal_Gal/UIHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoBo2D_Eyal_Gal
+{
+    public class UIHandler:IStartable
+    {
+        public UIHandler()
+        {
+            SubscriptionManager.AddSubscriber<IStartable>(this);
+        }
+        public void Start()
+        {
+
+        }
+        void CreateHealthBar()
+        {
+
+        }
+    }
+}
---
+++ BoBo2D_Eyal_Gal/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoBo2D_Eyal_Gal
+{
+    public static class UIManager
+    {
+        static UIHandler _uiHandler;
+        public static void AddHealth()
+        {
+
+        }
+        public static void ReduceHealth()
+        {
+
+        }
+        public static void AddMainWeaponAmmo()
+        {
+
+        }
+        public static void ReduceMainWeaponAmmo()
+        {
+
+        }
+    }
+}
