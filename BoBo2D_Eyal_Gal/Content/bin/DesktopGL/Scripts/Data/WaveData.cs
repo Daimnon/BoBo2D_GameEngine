8c00dc38919196dd3c4321360e0b02f6f836545c
Daimnon <galdrorerez@gmail.com>
2021-08-15 13:52:34 +0300

    Super Clean

---
+++ BoBo2D_Eyal_Gal/Content/bin/DesktopGL/Scripts/Data/WaveData.cs
@@ -0,0 +1,29 @@
+﻿namespace BoBo2D_Eyal_Gal
+{
+    public class Wave
+    {
+        #region Field
+        SpaceshipType _enemyShipType;
+
+        int _spawnMinWidth, _spawnMaxWidth, _numberOfEnemies;
+        #endregion
+
+        #region Properties
+        public SpaceshipType EnemyShipType => _enemyShipType;
+        public int SpawnMinWidth => _spawnMinWidth;
+        public int SpawnMaxWidth => _spawnMaxWidth;
+        public int NumberOfEnemies => _numberOfEnemies;
+        #endregion
+
+        #region Constructor
+        public Wave(int spawnMinWidth, int spawnMaxWidth, SpaceshipType enemyShipType, int numberOfEnemies)
+        {
+            _spawnMinWidth = spawnMinWidth;
+            _spawnMaxWidth = spawnMaxWidth;
+            _enemyShipType = enemyShipType;
+            _numberOfEnemies = numberOfEnemies;
+        }
+
+        #endregion
+    }
+}
