d9c59065c357c07133bdc17df510b9c7998e7527
Daimnon <49341329+Daimnon@users.noreply.github.com>
2021-08-18 06:41:19 +0300

    Fixed

--- BoBo2D_Eyal_Gal/Content/bin/DesktopGL/Scripts/Gals new project/Time.cs
+++ BoBo2D_Eyal_Gal/Content/bin/DesktopGL/Scripts/Gals new project/Time.cs
@@ -6,15 +6,20 @@ namespace BoBo2D_Eyal_Gal
 {
     public static class Time
     {
+        private readonly static int _frameCount;
         private readonly static float _fixedTime, _deltaTime, _fixedUnscaledDeltaTime;
-        public readonly static double _fixedTimeAsDouble;
+        private readonly static double _fixedTimeAsDouble, _fixedUnscaledTimeAsDouble;
+
         private static float _fixedDeltaTime;
 
+
+        public static int FrameCount => _frameCount;
         public static float FixedTime => _fixedTime;
         public static float DeltaTime => _deltaTime;
         public static float FixedDeltaTime => _fixedDeltaTime;
         public static float FixedUnscaledDeltaTime => _fixedUnscaledDeltaTime;
         public static double FixedTimeAsDouble => _fixedTimeAsDouble;
+        public static double FixedUnscaledTimeAsDouble => _fixedUnscaledTimeAsDouble;
 
     }
 }
